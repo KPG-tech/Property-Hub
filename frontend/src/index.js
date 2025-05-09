30276851bf3852b357d1f722fc97ef3c70877b42
Radeesa <radeesarandini3501@gmail.com>
2025-05-09 16:07:14 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+ 
 );
